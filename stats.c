bf6468aebd4c158e99c178875dfcaff97d7fad0b
vikas pandey <vip84389@gmail.com>
2021-01-06 20:25:39 +0530

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -35,9 +35,26 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+ 
+};
 
-}
+void print_statistics(unsigned char *ptr,unsigned int length){
+};
 
-/* Add other Implementation File Code Here */
+void print_array(unsigned char *ptr,unsigned int length){
+};
+
+unsigned char find_median(unsigned char *ptr,unsigned int length){
+};
+
+unsigned char find_mean(unsigned char *ptr,unsigned int length){
+};
+
+unsigned char find_maximum(unsigned char *ptr,unsigned int length){
+};
+
+unsigned char find_minimum(unsigned char *ptr,unsigned int length){
+};
+
+void sort_array(unsigned char *ptr,unsigned int length){
+};
--- stats.h
+++ stats.h
@@ -24,17 +24,93 @@
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief print the statistical value of given data
  *
- * <Add Extended Description Here>
+ * This function takes as input a array pointer
+ * and length of array.This will print the mean,
+ * median, maximum and minimum of given array data.
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param ptr Array pointer of data
+ * @param length Length of array data
  *
- * @return <Add Return Informaiton here>
+ * @return void
  */
-
-
+ void print_statistics(unsigned char *ptr,unsigned int length);
+/**
+ * @brief print the array on screen
+ *
+ * This function takes as input a array pointer and
+ * length of array.This will print the array on screen.
+ *
+ * @para ptr Array pointer of data
+ * @para length Length of array data
+ *
+ * @return void
+ */
+void print_array(unsigned char *ptr, unsigned int length);
+/**
+ * @brief calculate the median
+ *
+ * This function takes as input a array pointer and length
+ * of array.This will calculate the median of given data.
+ * It will return the value of median.
+ *
+ * @para ptr Array pointer of data
+ * @para length Length of array
+ *
+ * @return Value of median
+ */
+unsigned char find_median(unsigned char *ptr,unsigned int length);
+/**
+ * @brief calculate the mean
+ *
+ * This function take as input a array pointer and length
+ * of array. This will calculate the mean of given data.
+ * It will return the value of mean.
+ *
+ * @para ptr Array pointer of data
+ * @para length Length of array
+ *
+ * @return Value of mean
+ */
+unsigned char find_mean(unsigned char *ptr,unsigned int length);
+/**
+ * @brief find maximum value
+ *
+ * This function takes as input a array pointer and length
+ * of array. This will find the maximum value of given data. 
+ * It will return the maximum value.
+ *
+ * @para ptr Array pointer of data
+ * @para length Length of array
+ *
+ * @return maximum value
+ */
+unsigned char find_maximum(unsigned char *ptr,unsigned int length);
+/**
+ * @brief find minimum value
+ * 
+ * This function takes as input a array pointer and length
+ * of array. This will find the minimum value of given data.
+ * It will return the minimum value.
+ *
+ * @para ptr Array pointer of data
+ * @para length Length of array
+ *
+ * @return minimum value
+ */
+unsigned char find_minimum(unsigned char *ptr,unsigned int length);
+/**
+ * @brief sort the given array
+ *
+ * This function takes as input a array pointer and length 
+ * of array. This will sort the array from largest to smallest.
+ * It will return the sorted array.
+ *
+ * @para ptr Array pointer of data
+ * @para length Length of array
+ *
+ * @return void
+ */
+void sort_array(unsigned char *ptr,unsigned int length);
 #endif /* __STATS_H__ */
